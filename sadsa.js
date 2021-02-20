2f3ae3f69fb0ab37fce71ad45d9e0bfd0d829cc6
unknown <peretz.itay@gmail.com>
2021-02-20 02:34:40 +0200

    add send sms

---
+++ sadsa.js
@@ -0,0 +1,7 @@
+const date1 = new Date();
+
+setTimeout(() => {
+  const date2 = new Date();
+  const calc = date2 - date1;
+  console.log(calc < 3000);
+}, 3200);
--- src/cronJob.js
+++ src/cronJob.js
@@ -1,11 +1,24 @@
 const priceCompare = require("./priceCompare");
 const cron = require("node-cron");
 const email = require("./email");
+const sendSms = require("./sns_publishsms");
+
+let products = [];
+
+setInterval(() => {
+  products = [];
+}, 1000 * 60 * 60);
 
 const price_alert = async (productId) => {
   const [isLower, product] = await priceCompare(productId);
   if (isLower) {
+    if (products.includes(product)) {
+      console.log(`didnt send sms again  product:${product}`);
+      return;
+    }
     email(product);
+    sendSms(product);
+    products.push(product);
     console.log(`the price lower send email ${product}`);
   } else {
     console.log(`the price regular ${product}`);
@@ -14,7 +27,8 @@ const price_alert = async (productId) => {
 
 module.exports = () => {
   // Schedule tasks to be run on the server.
-  cron.schedule("*/10 * * * *", function () {
+  cron.schedule("*/10 * * * * *", function () {
+    // cron.schedule("*/10 * * * *", function () {
     console.log("running a task every 2 minutes");
     price_alert("B07B2X1VXZ");
     price_alert("B07KXCTRF6");
